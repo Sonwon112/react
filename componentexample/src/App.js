89b01fe967259456da4d21890ca6b8499cb494d9
Sonwon112 <tlatmsdkqk@gmail.com>
2023-03-10 09:42:53 +0900

    교수님 따라서 만든 예제

--- componentexample/src/App.js
+++ componentexample/src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
 
+function Header(){
+  return(
+    <header>
+      <h1><a href="">WEB</a></h1>
+    </header>
+  )
+}
+
+function Nav(){
+  return(
+    <nav>
+        <ol>
+          <li><a href = "/rr/1">html</a></li>
+          <li><a href = "/rr/2">css</a></li>
+          <li><a href = "/rr/3">javascript</a></li>
+        </ol>
+      </nav>
+  )
+}
+
+function Article(){
+  return(
+    <article>
+        <h2>환영!</h2>
+        Hello WEB programming
+      </article>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      test
+      <Header className = "First"/>
+      <Header className = "Second"/>
+      <hr/>
+      <Nav/>
+      <Article/>
+            
     </div>
   );
 }
