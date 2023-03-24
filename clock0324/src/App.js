34048ee7afb1f54200fc3cd2111453d1f29be523
Sonwon112 <tlatmsdkqk@gmail.com>
2023-03-24 09:44:17 +0900

    index.js에서 render함수를 setInterval하여 1초 단위로 시계가 표현되게 작업

---
+++ clock0324/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Clock from './component/Clock'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Clock name ="홍길동"/>
+    </div>
+  );
+}
+
+export default App;
--- viewexample0323/src/App.js
+++ viewexample0323/src/App.js
@@ -10,7 +10,7 @@ function App() {
     if(count%2 == 0){
       console.log("홀수");
     }else{
-      console.log("짝수");
+      console.log("짝수,");
     }
   }
 
