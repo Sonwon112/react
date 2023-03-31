e04ff97c7f9d3c2044e0e1e19ac2af89c6e89cd5
Sonwon112 <tlatmsdkqk@gmail.com>
2023-03-31 09:14:00 +0900

    클래스 컴포넌트 마무리

--- class_component0330/src/component/Box2.js
+++ class_component0330/src/component/Box2.js
@@ -5,14 +5,22 @@ export default class Box2 extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            city:'진주',
+            cnt:0
         };
     }
-  render() {
+    ff=()=>{
+        this.setState({cnt:this.state.cnt+1});
+    }
+
+    render() {
 
     return (
       <div>
-        <div className='box2'> 클릭횟수 {this.props.num}</div>
+        <h2>class Component name in,{this.props.num} in {this.state.city}</h2>
+        <hr/>
+        <button onClick={this.ff}>클릭!</button>
+        <div>클릭 회수 : {this.state.cnt}</div>
       </div>
       
       
