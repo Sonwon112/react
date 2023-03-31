e1467b2a4ee4ed7f23847d86147a1c5914cc04a0
Sonwon112 <tlatmsdkqk@gmail.com>
2023-03-31 12:55:49 +0900

    todoList-> 목록 추가 삭제 기능제작

---
+++ todolist0331/src/App.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+import './App.css';
+import TodoListBoaard from './component/TodoListBoaard';
+
+
+
+function App() {
+  const [inputText,setInputText] = useState("");
+  const [listData,setListData] = useState([]);
+  
+
+
+  const addData = ()=>{
+    setListData([...listData,inputText]);
+    document.getElementById()
+  }
+
+  const deleteData = (index)=>{
+    listData.splice(index,1);
+    setListData([...listData]);
+    //console.log(listData);
+  }
+
+  const clear = ()=>{
+    listData.splice(0);
+    setListData([...listData]);
+  }
+
+
+  return (
+    <div className="App">
+      <input id="todoListInput" onChange={event=>{setInputText(event.target.value)}}></input>
+      <button onClick={addData}>추가</button>
+      <button onClick={clear}>전체 삭제</button>
+      <hr/>
+      <TodoListBoaard data={listData} deleteData = {deleteData}/>
+    </div>
+  );
+}
+
+export default App;
