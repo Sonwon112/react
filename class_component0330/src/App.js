1b03e8c8333b1d02ae976f4c4d8cc3f3d27f320f
Sonwon112 <tlatmsdkqk@gmail.com>
2023-03-31 09:14:00 +0900

    클래스 컴포넌트 마무리

--- class_component0330/src/App.js
+++ class_component0330/src/App.js
@@ -1,28 +1,18 @@
-import React,{useState} from 'react';
-import { Component } from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Box from './component/Box';
 import Box2 from './component/Box2';
 
 function App() {
-  const [count,setCount] = useState(0);
-  const clickButton = ()=> {
-    setCount(count+1);
-    document.title = `클릭회수 : ${count}`;
-  }
+  //inc = ()=>{
 
-  inc = ()=>{
-    
-  }
+  //}
 
   return (
     <div className="App">
-      <div>클릭회수 : {count}</div>
-      <button onClick={clickButton}>클릭</button>
-      <hr/><hr/>
-      <Box num={count}/>
-      <button onClick={this.inc}>클릭2!</button>
-      <Box2 num={count}/>
+      <h1>Hello,ClassComponent</h1>
+      <hr/>
+      <Box2 num={'홍길동'}/>
     </div>
   );
 }
