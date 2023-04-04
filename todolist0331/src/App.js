11b7874c71be4ab22c3e7f189295b465a2fd05f4
Sonwon112 <tlatmsdkqk@gmail.com>
2023-04-04 19:29:06 +0900

    최종본으로 수정하여 변경

--- todolist0331/src/App.js
+++ todolist0331/src/App.js
@@ -1,24 +1,11 @@
 import React,{useState} from 'react';
-import './App.css';
-<<<<<<< HEAD
-import TodoListBoaard from './component/TodoListBoaard';
-
-
-=======
-import TodoListBoard from './component/TodoListBoard';
->>>>>>> 4668d13a983dd76a575ff5b332724aa35581e59e
+import './App.css'
+import TodoListBoard from './component/TodoListBoard'
 
 function App() {
   const [inputText,setInputText] = useState("");
   const [listData,setListData] = useState([]);
   
-
-<<<<<<< HEAD
-
-  const addData = ()=>{
-    setListData([...listData,inputText]);
-    document.getElementById()
-=======
   const addData = ()=>{
     if(!inputText == ''){
       setListData([...listData,inputText]);
@@ -26,23 +13,12 @@ function App() {
     }else{
       alert("텍스트를 입력하세요");
     }
->>>>>>> 4668d13a983dd76a575ff5b332724aa35581e59e
   }
 
   const deleteData = (index)=>{
     listData.splice(index,1);
     setListData([...listData]);
-<<<<<<< HEAD
-    //console.log(listData);
   }
-
-=======
-    
-    //console.log(listData);
-  }
-
-
->>>>>>> 4668d13a983dd76a575ff5b332724aa35581e59e
   const clear = ()=>{
     listData.splice(0);
     setListData([...listData]);
@@ -51,21 +27,15 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <input id="todoListInput" onChange={event=>{setInputText(event.target.value)}}></input>
-      <button onClick={addData}>추가</button>
-      <button onClick={clear}>전체 삭제</button>
-      <hr/>
-      <TodoListBoaard data={listData} deleteData = {deleteData}/>
-=======
+
       <input id="todoListInput" value={inputText} onChange={event=>{setInputText(event.target.value)}}></input>
       <button onClick={addData}>추가</button>
       <button onClick={clear}>전체 삭제</button>
       <hr/>
       <TodoListBoard data={listData} deleteData = {deleteData}/>
->>>>>>> 4668d13a983dd76a575ff5b332724aa35581e59e
     </div>
   );
+  
 }
 
-export default App;
+export default App
--- todolist0331/src/component/TodoListContent.js
+++ todolist0331/src/component/TodoListContent.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import '../App.css'
-<<<<<<< HEAD
-import App from '../App'
-
-
-
-const TodoListContent = (props) => {
-    const deleteListContent = ()=>{
-        props.deleteData(props.index);
-=======
 
 const TodoListContent = (props) => {
     
     const deleteListContent = ()=>{
         props.deleteData(props.index);
-        
->>>>>>> 4668d13a983dd76a575ff5b332724aa35581e59e
     }
 
     return (
